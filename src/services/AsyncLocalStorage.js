faa1195ef41c0e3d95c9dbea36aadbbc153acf71
mysql73 <arfan2173@gmail.com>
2021-12-31 21:49:14 +0700

    create an async way to storeto storage

---
+++ src/services/AsyncLocalStorage.js
@@ -0,0 +1,32 @@
+class AsyncLocalStorage {
+    static setItem(key, value) {
+        return Promise.resolve().then(() => {
+            if (typeof value != "string" ||
+                typeof value != "number") value = JSON.stringify(value);
+            window.localStorage.setItem(key, value);
+        });
+    }
+
+    static getItem(key) {
+        return Promise.resolve().then(() => {
+            let item = window.localStorage.getItem(key);
+            try {
+                return JSON.parse(item);
+            } catch (error) {
+                return item;
+            }
+        });
+    }
+
+    static removeItem(key) {
+        return Promise.resolve()
+            .then(() => window.localStorage.removeItem(key));
+    }
+
+    static clear() {
+        return Promise.resolve()
+            .then(() => window.localStorage.clear());
+    }
+}
+
+export default AsyncLocalStorage;
